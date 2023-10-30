4b0ad5dea84e12025341ed968cbc369e55599473
Samit Koyom <samitkoyom@gmail.com>
2023-10-31 02:31:40 +0700

    initial commit

---
+++ src/main/java/com/itgenius/springmvcmysql_jdb/SpringmvcmysqlJdbApplication.java
@@ -0,0 +1,13 @@
+package com.itgenius.springmvcmysql_jdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcmysqlJdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcmysqlJdbApplication.class, args);
+	}
+
+}
